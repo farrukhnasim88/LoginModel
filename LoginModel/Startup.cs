92b8b0993e004331afe14e00e584dd31a85691c7
FARRUKH Nasim <farrukh.nasim1@studytafensw.edu.au>
2020-07-23 23:35:24 +1000

    created identity layout page

--- LoginModel/Startup.cs
+++ LoginModel/Startup.cs
@@ -24,7 +24,7 @@ namespace LoginModel
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
